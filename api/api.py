6822b8b0b4ecf5f9a3b7e552f3823a6cd50569f7
SBhujangari <sai.bhujangari@gmail.com>
2020-10-03 13:04:08 -0700

    created file-upload

--- api/api.py
+++ api/api.py
@@ -1,9 +1,55 @@
 import time
-from flask import Flask
+import os
+from flask import Flask, request, flash, redirect, url_for, session, send_file
+from werkzeug.utils import secure_filename
 
+ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg", "gif"])
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+app.config["SECRET_KEY"] = "secretkey"
+app.config["UPLOADED_IMAGES_DEST"] = "uploads/images"
+app.config["MAX_CONTENT_LENGTH"] = 0.5 * 1024 * 1024
 
 
+#get method via just route
 @app.route('/time')
 def get_current_time():
 	return {'time': time.time()} 
+
+#Example using url params
+@app.route('/test', methods=['POST'])
+def test():
+	json = request.get_json()
+	return {'lang': json['language']} 
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+#Example using url params for file
+@app.route("/upload", methods=["POST"])
+def upload_image():
+    if "file" not in request.files:
+        flash("No file part")
+        return redirect(request.url)
+    file = request.files["file"]
+    if file.filename == "":
+        flash("Select An Image")
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        flash("Image successfully uploaded and displayed")
+        return {'test': 'test'}
+    else:
+        flash("We only accept png, jpg, jpeg, gif")
+        return redirect(request.url)
+
+@app.route("/display/<filename>")
+def display_image(filename):
+    filepath = 'static/uploads/' + filename
+    return send_file(filepath)
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
+    app.run(debug=True, port=5000)
--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [time, setTime] = useState(0)
+  const [url, setUrl] = useState(null)
+  let fileRef = useRef()
+  let textRef = useRef()
+
   useEffect(() => {
     fetch('/time').then((res) => res.json()).then(data => {
       setTime(data.time)
     })
+    const data = {
+      'language': 'Python'
+    }
+    fetch('/test', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    }).then((res) => res.json()).then(data => {
+      console.log('Test response', data)
+    })
   }, [])
+
+  const handleUploadImage = (ev) => {
+    ev.preventDefault();
+    console.log('handleUploadImage', fileRef.files[0], textRef.value)
+    const data = new FormData();
+    data.append('file', fileRef.files[0]);
+    data.append('filename', textRef.value);
+    const uploadAndGet = async () => {
+      await fetch('/upload', {
+        method: 'POST',
+        body: data,
+      })
+      const filepath = '/display/' + fileRef.files[0].name
+      await fetch(filepath).then((res) => setUrl(res.url))
+    }
+    uploadAndGet()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <p>Current Time: {time} </p>
-      </header>
+        <form onSubmit={handleUploadImage}>
+          <div>
+            <input ref={(ref) => { fileRef = ref; }} type="file" />
+          </div>
+          <div>
+            <input ref={(ref) => {textRef = ref; }} type="text" placeholder="Enter the desired name of file" />
+          </div>
+          <br />
+          <div>
+            <button type='submit'>Upload</button>
+          </div>
+        </form>
+        {
+          url && (
+            <img src={url} />
+          )
+        }
     </div>
   );
 }
